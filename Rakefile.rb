74db46b3c114863dc2243ba341108582924b4dcb
Ivan Zabrovskiy <loriowar@gmail.com>
2016-10-12 12:03:17 +0300

    Improvement by developers feedback
    - change logic of config work: implemented two separate configs for default and custom options
    - add support of rails env
    - add configuration of AR objects through same config
    - change a sequence of initialization of a rake environment: config goes first for use it in AR configuration

--- Rakefile.rb
+++ Rakefile.rb
@@ -7,10 +7,24 @@ StandaloneMigrations::Tasks.load_tasks
 # Dir.glob('lib/pg_migration/v1/**/*.rb').sort.each {|r| require File.expand_path(r, File.dirname(__FILE__))}
 require 'pg_migration/v1/base'
 
+# @note: config must be loaded before setup of AR parameters and before require modules
+# @note: for separate default values (which contain most part of parameters) from custom part (with less part of parameters)
+#        we use two config files and merge them, otherwise we can't make a deep merge or redefine certain keys inside an array
+#        within a single yml file
+CUSTOM_CONFIGURATION =
+    YAML.load_file(File.dirname(__FILE__) + '/db/default_soconfig.yml').
+        with_indifferent_access.
+        deep_merge(
+        YAML.load_file(File.dirname(__FILE__) + '/db/custom_soconfig.yml').with_indifferent_access[Rails.env]
+    )
+
+# @note: in model used values from config
 # Dir.glob('model/**/*.rb').each {|r| require r}
 require 'model/schema_migration_extension'
 
 ActiveRecord::Base.schema_format = :sql
-
-CUSTOM_CONFIGURATION =
-    YAML.load_file(File.dirname(__FILE__) + '/db/custom_configuration.yml').with_indifferent_access
+# @note: alternative table name for AR table with information about migration; aim: prevent intersection with default one
+ActiveRecord::Base.schema_migrations_table_name = CUSTOM_CONFIGURATION.dig(:active_record_object, :table, :schema_migrations)
+# @note: disable foolproof, appeared in Rails 5: https://github.com/rails/rails/pull/22967
+ActiveRecord::Base.internal_metadata_table_name = CUSTOM_CONFIGURATION.dig(:active_record_object, :table, :internal_metadata_table_name)
+ActiveRecord::Base.protected_environments = CUSTOM_CONFIGURATION.dig(:active_record_object, :option, :protected_environments)
--- lib/model/schema_migration_extension.rb
+++ lib/model/schema_migration_extension.rb
@@ -1,5 +1,6 @@
 module Model
   class SchemaMigrationExtension < ActiveRecord::Base
+    self.table_name = ::CUSTOM_CONFIGURATION.dig(:active_record_object, :table, :schema_migration_extensions)
     self.primary_key = 'version'
 
     serialize :configuration, Hash
